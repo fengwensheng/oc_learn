bd150cd6391f9b3c6da8006532c3b422fe630584
Vincent Feng <fengwenshenging@gmail.com>
2022-01-11 23:12:50 +0800

    Initial Commit

---
+++ oc_learn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  oc_learn
+//
+//  Created by Vincent Feng on 2022/1/11.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
